39589856bdcf998230fc45b7c1d3b6db50722cc9
J. Paul Duplantis <paulduplantis@gmail.com>
2024-12-21 23:53:52 -0500

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const SUPABASE_URL = "https://qednuirrccgrlcqrszmb.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFlZG51aXJyY2NncmxjcXJzem1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ0MTA5NDYsImV4cCI6MjA0OTk4Njk0Nn0.Lb9OmaJN5TU_AOSoExbHLTBpCYcURTT3lG2bn1RJEr0";
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
+console.log('Supabase client initialized:', supabase);
 async function fetchData() {
     console.log('Function fetchData is running...'); // Step 1
     try {
